788cf8f1e1b4d6b93adad1073a85a7da8601b329
rayhanashlikh <rayhanind24@gmail.com>
2023-09-13 11:58:23 +0700

    add dependencies, migration & seeder, and move assets to public

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+                // \App\Models\User::factory()->create([
+        //     'kode' => rand(10,100));
+        //     'name' => 'arif',
+        //     'email' => 'arif@gmail.com',
+        //     'password' => bcrypt('password'),
+        // ]);
+
+        $this->call([
+            PermissionSeeder::class,
+            KursusSeeder::class,
+            SectionSeeder::class,
+            ArtikelSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/KursusSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KursusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('kursus')->insert([
+            'judul' => 'Web Developer',
+            'author' => 'Bengkel Koding',
+            'hari' => 'Senin - Jumat',
+            'jam' => '09.30 - 12.00 / 12.30 - 15.00',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('kursus')->insert([
+            'judul' => 'Mobile Developer',
+            'author' => 'Fahri Firdausillah, Syaifur Rohman, dkk',
+            'hari' => 'Senin - Jumat',
+            'jam' => '09.30 - 12.00 / 12.30 - 15.00',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('kursus')->insert([
+            'judul' => 'Data Science',
+            'author' => 'Bengkel Koding',
+            'hari' => 'Senin - Jumat',
+            'jam' => '09.30 - 12.00 / 12.30 - 15.00',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('kursus')->insert([
+            'judul' => 'Computer Vision',
+            'author' => 'Bengkel Koding',
+            'hari' => 'Senin - Jumat',
+            'jam' => '09.30 - 12.00 / 12.30 - 15.00',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('kursus')->insert([
+            'judul' => 'Game Programming',
+            'author' => 'Bengkel Koding',
+            'hari' => 'Senin - Jumat',
+            'jam' => '09.30 - 12.00 / 12.30 - 15.00',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+    }
+}
