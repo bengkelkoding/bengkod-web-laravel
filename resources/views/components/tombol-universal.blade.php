1a72f2783e4346b920527f5975efd00f9eec6bb4
muhammadroynaldi <111202113471@mhs.dinus.ac.id>
2023-09-14 10:59:23 +0700

    slicing dashboard mahasiswa

--- resources/views/components/tombol-universal.blade.php
+++ resources/views/components/tombol-universal.blade.php
@@ -1,3 +1,3 @@
-<a {{ $attributes->merge(['class'=>'bg-[#114D91] mt-4 py-1 rounded-md text-white flex justify-center items-center text-xl font', 'href'=>'/']) }}>
+<a {{ $attributes->merge(['class'=>'bg-[#114D91] mt-4 py-1 rounded-md text-white flex justify-center items-center text-xl font hover:bg-cyan-500', 'href'=>'/']) }}>
     <button>{{ $slot }}</button>
 </a>
--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -10,18 +10,48 @@
         </div>
     </div>
 
-    <div class="box-border h-40 w-[700px] p-1 border-4 ml-[190px] mt-12">
-        <h3 class="text-black font-bold ml-2">Kursus Anda</h3>
-        <div class="box-border h-auto w-[662px] shadow-lg ml-2 flex justify-center items-center ">
-            <div class="mr-5">
-                <img src="assets\admin\backgrounds\web-dev 1.png" alt="" class="">
-            </div>
-            <div class="mr-20">
-                <x-tombol-universal class="w-[180px] h-auto">Belajar Sekarang</x-tombol-universal>
+    <div class="box-border h-40 w-[700px] p-1 border ml-[190px] mt-12">
+        <h3 class="text-black font-bold ml-2 mb-2 text-[14px]">Kursus Anda</h3>
+        <div class="box-border h-auto w-[662px] shadow-lg ml-2 flex justify-between items-center">
+            <div class="mr-5 p-2 flex">
+                <img src="assets\admin\backgrounds\web-dev 1.png" alt="" >
+                <div class="w-[170px] h-auto pl-5">
+                    <h1 class="text-black font-bold text-[20px]">Web Developer</h1>
+                    <p class="text-[#828282] text-[12px]">
+                        <img src="assets\admin\icons\users-solid.png" alt="" class="inline mr-2">
+                        10 Mahasiswa Terdaftar</p>
+                    <p class="text-[#828282] text-[12px]">
+                        <img src="assets\admin\icons\calendar-days-solid.png" alt="" class="inline mr-2">
+                        Senin - Jum'at</p>
+                    <p class="text-[#828282] text-[12px]">
+                        <img src="assets\admin\icons\clock-solid.png" alt="">
+                        09.30 - 12.00 / 12.30 - 15.00
+                    </p>
+                </div>
             </div>
             <div>
-                <x-tombol-universal class="w-[180px] h-auto">Belajar Sekarang</x-tombol-universal>
+                <x-tombol-universal class="w-[180px] h-auto mr-5 mb-5"><span class="text-[14px]">Belajar Sekarang</span></x-tombol-universal>
             </div>
         </div>
     </div>
+
+    <div class="box-border h-40 w-[700px] p-1 border ml-[190px] mt-12">
+        <h3 class="text-black font-bold ml-2 mb-2 text-[14px]">Kursus Anda</h3>
+        <div class="box-border h-auto w-[662px] shadow-lg ml-2 flex justify-between items-center">
+            <div class="mr-5 p-2 flex">
+                <img src="assets\admin\backgrounds\web-dev 1.png" alt="" >
+                <div class="w-[170px] h-auto pl-5">
+                    <h1 class="text-black font-bold text-[20px]">Web Developer</h1>
+                    <p class="text-[#828282] text-[12px]">10 Mahasiswa Terdaftar</p>
+                    <p class="text-[#828282] text-[12px]">Senin - Jum'at</p>
+                    <p class="text-[#828282] text-[12px]">09.30 - 12.00 / 12.30 - 15.00</p>
+                </div>
+            </div>
+            <div>
+                <x-tombol-universal class="w-[180px] h-auto mr-5 mb-5"><span class="text-[14px]">Belajar Sekarang</span></x-tombol-universal>
+            </div>
+        </div>
+    </div>
+
+
 </x-app-layout>
