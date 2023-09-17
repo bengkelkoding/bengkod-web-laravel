ad4b2b1f8411ba9e6f58b8cfd6c7383ee720d545
arifsptra <arif.sptrra@gmail.com>
2023-09-17 09:06:08 +0700

    improve view detail course

--- resources/views/kursus/detail.blade.php
+++ resources/views/kursus/detail.blade.php
@@ -1,18 +1,17 @@
-
 <x-universal-layout>
     <div class="pb-3 box-border w-full flex justify-center items-center max-md:justify-center flex-wrap">
-        <div class="mr-5 p-2 flex flex-wrap max-md:justify-center max-md:mr-0  mt-3">
+        <div class="mr-5 p-2 flex flex-wrap max-md:justify-center max-md:mr-0  my-10">
             <img src="{{ asset($kursus->image) }}" alt="Gambar Kursus" width="200px" height="200px" class="rounded">
-            <div class=" h-auto pl-5">
-                <h1 class="text-black font-bold text-[50px] max-md:text-center">{{ $kursus->judul }}</h1>
-                <p class="text-[#828282] text-[12px]">
+            <div class=" h-auto ml-[40px]">
+                <h1 class="text-black font-semibold text-[40px] max-md:text-center">{{ $kursus->judul }}</h1>
+                <p class="text-[#828282] text-[14px]">
                     <img src="assets\admin\icons\users-solid.png" alt="" class="inline mr-2">
                     {{ $kursus->users_count }} Mahasiswa Terdaftar</p>
-                <p class="text-[#828282] text-[12px]">
+                <p class="text-[#828282] text-[14px]">
                     <img src="assets\admin\icons\calendar-days-solid.png" alt="" class="inline mr-2">
                     {{ $kursus->hari }}
                 </p>
-                <p class="text-[#828282] text-[12px]">
+                <p class="text-[#828282] text-[14px]">
                     <img src="assets\admin\icons\clock-solid.png" alt="" class="inline mr-2">
                     {{ $kursus->jam }}
                 </p>
@@ -30,7 +29,7 @@
                         <form action="{{ route('update.kursus') }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin mendaftar ke kursus ini?')">
                             @csrf
                             <input type="hidden" name="kursus_id" value="{{ $kursus->id }}">
-                            <button type="submit" class="btn btn-primary">Daftar Kursus</button>
+                            <x-tombol-universal type="submit" class="px-6 h-auto mr-6 max-md:mr-0 mb-5">Daftar Kursus</x-tombol-universal>
                         </form>
                     @endif
                 @else
@@ -41,19 +40,19 @@
         </div>
     </div>
 
-    <div class="min-h-[60vh] w-screen px-4 flex justify-center mx-auto border-y ">
-        <div class="justify-start w-[50%] pr-8 pb-4 border-r ">
-            <h1 class="text-[32px] font-bold text-center mb-2 ml-2 mt-3">Deskripsi</h1>
+    <div class="w-screen px-4 flex justify-center mx-auto border-y ">
+        <div class="justify-start w-[50%] pb-10 pt-4 px-8 border-r ">
+            <h1 class="text-[28px] font-medium text-center mb-2 ml-2 mt-3">Deskripsi</h1>
             <p class="text-[14px] text-justify ml-2">{{ $kursus->description }}</p>
         </div>
 
-        <div class="box-border ml-3 w-[30%] pb-4 pl-5">
-            <h1 class="text-[32px] font-bold text-center mb-2 ml-2 mt-3">Tools</h1>
+        <div class="box-border ml-3 w-[30%] pb-10 pt-4 px-8">
+            <h1 class="text-[28px] font-medium text-center mb-2 ml-2 mt-3">Tools</h1>
             <ol class="list-disc ml-5">
                 <li >VS Code</li>
                 <li >Browser</li>
                 <li >Web Server</li>
-                </ol>
+            </ol>
         </div>
     </div>
 </x-universal-layout>
