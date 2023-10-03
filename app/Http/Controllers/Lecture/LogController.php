168de84a9b1980d0459e2148f83d2a7b6c3c2a4b
samuelandrey <samuelandrey2021@gmail.com>
2023-10-03 15:36:51 +0700

    Add dosens show logs students
    Signed-off-by: samuelandrey <samuelandrey2021@gmail.com>

---
+++ app/Http/Controllers/Lecture/LogController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Lecture;
+
+use App\Http\Controllers\Controller;
+use App\Models\Log;
+use App\Models\Tugas;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $user = auth()->user();
+        $search = $request->search ?? "";
+        $per_page = $request->per_page ?? 10;
+        $logs = Log::with('mahasiswa')
+            ->with('kursus')
+            ->where('id_kursus', $user->id_kursus)
+            ->orderByDesc('created_at')
+            ->paginate($per_page);
+
+        return view('lecture.log.index', compact('logs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            // dd($request->nilai);
+            $log = Log::find($id);
+            $log->update([
+                'status' => 1,
+            ]);
+            return response()->redirectToRoute('lecture.log.index');
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
