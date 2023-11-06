16b1222fe1d5239865b9f189cb6997d5ca4ef6ca
Hypes-Astro <muhamadalifanwar@gmail.com>
2023-11-06 12:42:24 +0700

    re slicing detail -3

--- resources/views/kursus/detail.blade.php
+++ resources/views/kursus/detail.blade.php
@@ -1,21 +1,22 @@
 <x-universal-layout>
-    <div class="pb-3  bg-blue-50 box-border w-full flex justify-center items-center max-md:justify-center flex-wrap">
+    <div
+        class="border-box flex max-xl:flex-col p-5 items-center justify-evenly mx-12 max-xl:mx-0 bg-blue-50 rounded-3xl h-[400px] max-md:h-auto my-10">
         <div class="mr-5 p-2 flex flex-wrap max-md:justify-center max-md:mr-0 my-10">
             <img src="{{ asset($kursus->image) }}" alt="Gambar Kursus" width="200px" height="200px"
                 class="rounded-lg sm:mb-2 mr-4">
             <div class="h-auto ml-[40px] max-md:ml-0">
                 <h1 class="text-black font-semibold text-[40px] max-md:text-left xs:text-center">{{ $kursus->judul }}
                 </h1>
-                <p class="text-[#828282] text-[14px] ">
+                <p class="text-[#828282] text-[14px] xs:text-center md:text-left">
                     <img src="{{ asset('assets\admin\icons\users-solid.png') }}" alt="" class="inline mr-2 ">
                     {{ $kursus->users_count }} Mahasiswa Terdaftar
                 </p>
-                <p class="text-[#828282] text-[14px] ">
+                <p class="text-[#828282] text-[14px] xs:text-center md:text-left">
                     <img src="{{ asset('assets\admin\icons\calendar-days-solid.png') }}" alt=""
                         class="inline mr-2">
                     {{ $kursus->hari }}
                 </p>
-                <p class="text-[#828282] text-[14px]">
+                <p class="text-[#828282] text-[14px] xs:text-center md:text-left">
                     <img src="{{ asset('assets\admin\icons\clock-solid.png') }}" alt="" class="inline mr-2">
                     {{ $kursus->jam }}
                 </p>
@@ -23,7 +24,7 @@
             </div>
             {{-- page button --}}
             <div
-                class="p-3 lg:ml-5 box-border w-[20rem] h-[12rem] max-md:h-24 bg-white flex flex-col max-xl:flex-row  items-center justify-center rounded-2xl">
+                class="p-3 lg:ml-5 box-border w-[20rem] h-[12rem] max-md:h-24 xs:mt-4 bg-white flex flex-col max-xl:flex-row  items-center justify-center rounded-2xl">
                 @auth
                     <!-- <p>{{}}</p> -->
                     @if (!is_null(auth()->user()->id_kursus) || auth()->user()->name == 'admin')
@@ -72,7 +73,7 @@
             <h1 class="text-[28px] font-medium text-center mb-2 ml-2 mt-3">Bab Yang Akan Dipelajari</h1>
             <ol class="list-disc ml-8">
                 @foreach ($tools as $tool)
-                    <li>{{ $tool }}</li>
+                    <li class="bg-blue-50 my-3 rounded-full p-3">{{ $tool }}</li>
                 @endforeach
             </ol>
         </div>
