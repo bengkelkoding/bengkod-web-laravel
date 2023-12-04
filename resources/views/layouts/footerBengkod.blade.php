bc9134c9e8a05430a1cb967a08f2fbbedd79e430
arifsptra <arif.sptrra@gmail.com>
2023-12-04 21:48:54 +0700

    update footer

--- resources/views/layouts/footerBengkod.blade.php
+++ resources/views/layouts/footerBengkod.blade.php
@@ -1,7 +1,6 @@
-<footer class="w-full border-2 max-xl:h-auto flex flex-col ">
-    {{-- <p class="text-[#828282] ml-8 my-3  max-xl:mb-9 cursor-default ">Copyright © Bengkel Koding 2023 All rights reserved
-    </p> --}}
-    <div class="md:flex md:justify-between sm:px-12 px-4 py-7 mx-5 border-t-2 mb-7 ">
+<footer class="w-full flex flex-col border-t-2">
+    <!-- <p class="text-[#828282] ml-8 my-3  max-xl:mb-9 cursor-default">Copyright © Bengkel Koding 2023 All rights reserved</p> -->
+    <!-- <div class="md:flex md:justify-between sm:px-12 px-4 py-7 mx-5 border-t-2 mb-7 ">
         <div class="border-box">
             <h1 class="font-semibold mb-2">Bengkel Koding</h1>
             <div class="mb-5">
@@ -22,5 +21,15 @@
             </a>
         </div>
     </div>
-    <p class="text-center mt-10 pb-10">Bengkel Koding © 2023 All rights reserved.</p>
+    <p class="text-center mt-10 pb-10">Bengkel Koding © 2023 All rights reserved.</p> -->
+    <div class="container px-0 lg:px-6 2xl:px-0 mx-auto flex justify-between items-center py-4 text-[#828282] flex-col text-center gap-4 md:flex-row">
+        <div class="flex gap-2 items-center">
+            <img src="assets\img\Udinus_logo.png" alt="" class="h-10">
+            <div class="h-10 w-0.5 bg-slate-300 rounded-lg"></div>
+            <img src="assets\img\logo-bengkod-main.png" alt="" class="h-6">
+            <p>Bengkel Koding</p>
+        </div>
+        <!-- </div> -->
+        <p>Bengkel Koding © 2023 All rights reserved.</p>
+    </div>
 </footer>
