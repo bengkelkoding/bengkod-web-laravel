d9a236ab4302bc1b494cb312492aede6007904c6
arifsptra <arif.sptrra@gmail.com>
2023-12-04 21:50:42 +0700

    add style for main color and use material symbols

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,31 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
         "./node_modules/tw-elements/dist/js/**/*.js",
     ],
-    darkMode: '',
+    darkMode: "",
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["FigtrePope", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "primary-color": "#114D91",
+                "secondary-color": "var(--secondary-color)",
             },
         },
     },
 
-    plugins: [forms,require('tailwind-scrollbar-hide'),require("tw-elements/dist/plugin.cjs")],
+    plugins: [
+        forms,
+        require("tailwind-scrollbar-hide"),
+        require("tw-elements/dist/plugin.cjs"),
+    ],
 };
